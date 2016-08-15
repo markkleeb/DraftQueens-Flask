4dd40327d6ddb2e40dde225af911e92f4d7693e7
markkleeb <kleeb930@gmail.com>
2016-08-15 18:29:00 -0400

    enter users

--- app.py
+++ app.py
@@ -1,58 +1,134 @@
 import os
-from flask import Flask, render_template, redirect, abort
+from flask import Flask, render_template, redirect, abort, request
 from flask.ext.pymongo import PyMongo
 
 import models
+from models import User, Queen, db
 from flask.ext.sqlalchemy import SQLAlchemy
 from flask_googlelogin import GoogleLogin
+from flask_oauth import OAuth
+
+DEBUG = True
+SECRET_KEY = 'development key'
 
 
 app = Flask(__name__)
+app.debug = DEBUG
+app.secret_key = SECRET_KEY
+oauth = OAuth()
 
 #Configure SQLAlchemy Database
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
-db = SQLAlchemy(app)
+db.create_all()
 #mongo = PyMongo(app)
 
 
-#Configure Google Login API
-googlelogin = GoogleLogin(app)
 
-from flask_login import LoginManager
-login_manager = LoginManager()
-login_manager.init_app(app)
-googlelogin = GoogleLogin(app, login_manager)
-
-
-
-#Index Page
-@app.route('/', methods=['GET'])
+#Index Page (not yet set up)
+@app.route('/')
 def index():
-	return render_template("index.html")
+   users = User.query.all()
+   queens = Queen.query.order_by(Queen.name)
+
+   templateData= {
+   'users' : users,
+   'queens' : queens
+   }
+   return render_template("index.html", **templateData)
+ 
+
+ 
+
+#Form to add new users and queens
+@app.route('/adduser')
+def adduser():
+	users = User.query.all()
+	queens = Queen.query.all()
+
+	
+	
+	templateData = {
+		'users' : users,
+		'queens':queens
+		
+	}
+
+	return render_template("adduser.html", **templateData)
 
 
-#Copied from Flask-Google Tutorial
-@app.route('/oauth2callback')
-@googlelogin.oauth2callback
-def create_or_update_user(token, userinfo, **params):
-    user = User.filter_by(google_id=userinfo['id']).first()
-    if user:
-        user.name = userinfo['name']
-        user.avatar = userinfo['picture']
-    else:
-        user = User(google_id=userinfo['id'],
-                    name=userinfo['name'],
-                    avatar=userinfo['picture'])
-    db.session.add(user)
-    db.session.flush()
-    login_user(user)
-    return redirect(url_for('index'))
+#Post new User data
+@app.route('/newuser', methods=['POST'])
+def newuser():
+	u = request.form.get('username')
+	p = request.form.get('password')
+	e = request.form.get('email')
+	g = request.form.get('group')
+	
+	new = User(u, p, e, g, 0)
+	db.session.add(new)
+	db.session.commit()
+
+	users = User.query.all()
+	queens = Queen.query.all()
+
+	templateData = {
+		'users': users,
+		'queens': queens,
+	}
+
+	return render_template("adduser.html", **templateData)
+
+
+#Post new Queen data
+@app.route('/newqueen', methods=['POST'])
+def newqueen():
+	n = request.form.get('name')
+	d = request.form.get('description')
+	i = request.form.get('img')
+	
+	new = Queen(n, i, d, 1, 0, 0, 0)
+	db.session.add(new)
+	db.session.commit()
+
+	users = User.query.all()
+	queens = Queen.query.all()
+
+	templateData= { 'users':users,
+	'queens' : queens
+
+	}
+	return render_template("adduser.html", **templateData)
+
+
+#Clear all users
+@app.route('/clearall')
+def clearall():
+
+ 	users = User.query.all()
+
+ 	for u in users:
+ 		db.session.delete(u)
+ 		db.session.commit()
+
+	return redirect('/')
 
 
 
+#Clear specific User
+@app.route('/clear/<id>')
+def clearone(id):
 
-@app.route('/profile', methods=['GET'])
-#@login_required
-def profile():
-    return render_template("index.html")
+	
+	user = User.query.filter_by(id=id).first()
+	db.session.delete(user)
+	db.session.commit()
 
+
+	return redirect('/')
+ 
+ 
+def main():
+    app.run()
+ 
+ 
+if __name__ == '__main__':
+    main()
--- models.py
+++ models.py
@@ -1,13 +1,59 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
 from flask.ext.wtf import Form
-from wtforms.fields import StringField, SubmitField
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
+db = SQLAlchemy(app)
 
 
+class User(db.Model):
 
-class User(Form):
+	id = db.Column(db.Integer, primary_key=True)
+	username = db.Column(db.String(80), unique=True)
+	password = db.Column(db.String(80), unique=True)
+	email = db.Column(db.String(120), unique=True)
+	group = db.Column(db.Integer)
+	points = db.Column(db.Integer)
+	#queen1 = db.relationship('Queen', backref=db.backref('owners', lazy='dynamic'))
+	#queen2 = db.relationship('Queen', backref=db.backref('owners', lazy='dynamic'))
+	#queen3 = db.relationship('Queen', backref=db.backref('owners', lazy='dynamic'))
 
-	username = StringField(u'Name')
-	password = StringField(u'Password')
-	email = StringField
+	def __init__(self, username, password, email, group, points):
+		self.username = username
+		self.password = password
+		self.email = email
+		self.group = group
+		self.points = points
+        
+        
+
+	def __repr__(self):
+		return '<User %r>' % self.username
+
+
+class Queen(db.Model):
+
+	id = db.Column(db.Integer, primary_key=True)
+	name = db.Column(db.String(120))
+	img = db.Column(db.Text)
+	description = db.Column(db.Text)
+	active = db.Column(db.Boolean)
+	drafted = db.Column(db.Boolean)
+	kickedoff = db.Column(db.Boolean)
+	points = db.Column(db.Integer)
+
+	def __init__(self, name, img, description, active, points, drafted, kickedoff):
+		self.name = name
+		self.img = img
+		self.description = description
+		self.active = active
+		self.points = points
+		self.drafted = drafted
+		self.kickedoff = kickedoff
+        
+	def __repr__(self):
+		return '<User %r>' % self.name
 
 
 
